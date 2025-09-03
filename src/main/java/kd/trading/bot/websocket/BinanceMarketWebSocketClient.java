f7e105bbc77a0b8c396134fbf2f833259245a22e
Kiszel Dániel <daniel.kiszel@areus.hu>
2025-09-03 06:38:23 +0200

    Update BinanceMarketWebSocketClient.java

--- src/main/java/kd/trading/bot/websocket/BinanceMarketWebSocketClient.java
+++ src/main/java/kd/trading/bot/websocket/BinanceMarketWebSocketClient.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.framing.PingFrame;
 import org.java_websocket.handshake.ServerHandshake;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.net.URI;
 import java.util.concurrent.Executors;
@@ -32,34 +33,22 @@ public class BinanceMarketWebSocketClient extends WebSocketClient {
         this.listener = listener;
     }
 
-    public void sendPingFrame() {
+    @Scheduled(fixedRate = 1 * 60 * 1000) // 15 percenként
+    public void keepAlive() {
         if (this.isOpen()) {
-            try {
-                this.sendFrame(new PingFrame());
-                log.debug("Ping frame sent");
-            } catch (Exception e) {
-                log.warn("Ping failed", e);
-            }
+            this.sendPing();
+            log.debug("Sent PING to Binance WS");
         }
     }
 
     @Override
     public void onOpen(ServerHandshake handshakeData) {
         log.info("Connected to Binance Futures Market WebSocket");
-
-        // start ping/pong to keep alive
-        scheduler.scheduleAtFixedRate(() -> {
-            try {
-                if (this.isOpen()) this.sendPingFrame();
-            } catch (Exception e) {
-                log.warn("Failed to send ping", e);
-            }
-        }, PING_INTERVAL_MS, PING_INTERVAL_MS, TimeUnit.MILLISECONDS);
     }
 
     @Override
     public void onMessage(String message) {
-        log.info("RAW WS MESSAGE: {}", message); // railwayen is látni fogod
+        log.info("Raw message received WS Market"); // railwayen is látni fogod
         if(TelegramCommandService.SLEEP_MODE) return;
         long now = System.currentTimeMillis();
 
